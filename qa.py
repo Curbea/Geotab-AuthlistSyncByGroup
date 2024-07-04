b8cc288ff3d6c0199a5c30004ea75e368e7ea022
Curbea <Curtisdbpc@outlook.com>
2024-07-03 19:06:23 -0700

    Update qa.py

--- qa.py
+++ qa.py
@@ -9,19 +9,16 @@ api = API(username, password, database)
 def search_texts_authlistcontent(api):
     try:
         now_utc = datetime.now(timezone.utc) - timedelta(days=1)
-        logging.info(f"time {now_utc}")  # Correct logging usage
+        logging.info(f"time {now_utc}")
         texts = api.get('TextMessage', search={"fromDate": now_utc}) # Can't seem to combine search with messagecontent or device id
         driver_auth_texts = [text for text in texts if text.get('messageContent', {}).get('contentType') == "DriverAuthList"] # to do find message delivery faliure and add.
+        #Filter to last unique message sent to prevent spam
 
-        logging.info(f"Users: {json.dumps(driver_auth_texts, default=str)}")
-        return driver_auth_texts
-###We should just be able to pass faliures directly below
-def send_text_message(api, vehicles_to_update, Keys, add=True):
-       try:
-          logging.info(f"Keys {Keys}")
-          for key in Keys:
-               logging.info(f"Key {key}")
-               data = {
+#        logging.info(f"Users: {json.dumps(driver_auth_texts, default=str)}")
+        logging.info(f"Keys {Keys}")
+            for key in Keys:
+                 logging.info(f"Key {key}")
+                 data = {
 
     "device": {
         "id": "b1B5"
@@ -35,10 +32,10 @@ def send_text_message(api, vehicles_to_update, Keys, add=True):
     }
 }
 
-               api.add("TextMessage",data)
-               action = "added to" if add else "removed from"
-               logging.info(f"Keys {action} vehicle with ID: {vehicles_to_update}")
-       except Exception as e:
-           logging.error(f"Error sending text message to vehicle with ID: {vehicles_to_update}: {e}")
-#For loop over driver auth texts
+                 api.add("TextMessage",data)
+                 action = "added to" if add else "removed from"
+                 logging.info(f"Keys {action} vehicle with ID: {vehicles_to_update}")
+         except Exception as e:
+             logging.error(f"Error sending text message to vehicle with ID: {vehicles_to_update}: {e}")
+#For loop over vehicle id's
 #Maybe check after like an hour if the vehicle should be driving? (recieving messages)
